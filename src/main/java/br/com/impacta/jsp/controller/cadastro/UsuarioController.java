eba4a82bbfc1e234b49b3e3acbe3ed836bdafe98
alex <alexmbcdo@gmail.com>
2022-07-07 23:02:43 -0300

    alterando variavel do bd

--- src/main/java/br/com/impacta/jsp/controller/cadastro/UsuarioController.java
+++ src/main/java/br/com/impacta/jsp/controller/cadastro/UsuarioController.java
@@ -4,14 +4,19 @@ package br.com.impacta.jsp.controller.cadastro;
 import br.com.impacta.jsp.model.Usuarios;
 import br.com.impacta.jsp.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
 @Controller
 @RequestMapping("/usuario")
@@ -22,17 +27,32 @@ public class UsuarioController {
 
     @GetMapping
     public ModelAndView getCadastro() {
-        return new ModelAndView("/cadastro/cadastro-usuario");
+        ModelAndView modelAndView = new ModelAndView("/cadastro/cadastro-usuario");
+        modelAndView.addObject("usuario", new Usuarios());
+        return modelAndView;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public RedirectView salvarUsuario(Usuarios usuarios) {
+    public ModelAndView salvarUsuario(@Valid @ModelAttribute("usuario") Usuarios usuarios, Errors errors) {
+        if (errors.hasErrors()) {
+            return new ModelAndView("/cadastro/cadastro-usuario");
+        }
 
-        String senhaUsuario = usuarios.getSenha();
-        String senhaCriptografada = new BCryptPasswordEncoder().encode(senhaUsuario);
-        usuarios.setSenha(senhaCriptografada);
-        usuarioService.salvar(usuarios);
+        try {
 
-        return new RedirectView("/listagem");
+            String senhaUsuario = usuarios.getSenha();
+
+            String senhaCriptografada = new BCryptPasswordEncoder().encode(senhaUsuario);
+            usuarios.setSenha(senhaCriptografada);
+            usuarioService.salvar(usuarios);
+        }catch (DataIntegrityViolationException d){
+
+            errors.rejectValue("email", "400", "email já utilizado no cadastro");
+            return new ModelAndView("/cadastro/cadastro-usuario");
+        }
+
+
+        return new ModelAndView("redirect:/listagem");
     }
+
 }
--- src/main/webapp/WEB-INF/views/cadastro/cadastro-usuario.jsp
+++ src/main/webapp/WEB-INF/views/cadastro/cadastro-usuario.jsp
@@ -1,4 +1,5 @@
 <%@page pageEncoding="UTF-8" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="spring"%>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -11,7 +12,7 @@
 <nav class="navbar navbar-inverse navbar-static-top"></nav>
 
 
-<form class="form-horizontal" method="post" action="${pageContext.request.contextPath}/usuario" >
+<spring:form class="form-horizontal" method="post" action="${pageContext.request.contextPath}/usuario" modelAttribute="usuario">
     <div class="panel panel-default">
         <div class="panel-heading">
             <h1 class="panel-title">Cadastro de usuario</h1>
@@ -20,19 +21,22 @@
     <div class="form-group">
         <label for="nome" class="col-sm-2 control-label">Nome</label>
         <div class="col-sm-4">
-           <input type="text" class="form-control" id="nome" name="nome"/>
+           <spring:input type="text" class="form-control" id="nome" name="nome" path="nome"/>
+           <spring:errors path="nome" cssClass="error" />
         </div>
     </div>
     <div class="form-group">
         <label for="data" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-2">
-            <input type="email" class="form-control" id="email" name="Email" />
+            <spring:input type="email" class="form-control" id="email" name="email" path="email"/>
+            <spring:errors path="email" cssClass="error" />
         </div>
     </div>
     <div class="form-group">
         <label for="senha" class="col-sm-2 control-label">Senha</label>
         <div class="col-sm-2">
-            <input type="password" class="form-control" id="senha" name="senha" />
+            <spring:input type="password" class="form-control" id="senha" name="senha" path="senha"/>
+            <spring:errors path="senha" cssClass="error" />
         </div>
     </div>
     <div class="form-group">
@@ -41,6 +45,6 @@
         </div>
     </div>
     	<script src="${pageContext.request.contextPath}/js/bootstrap.min.js"></script>
-</form>
+</spring:form>
 </body>
 </html>
